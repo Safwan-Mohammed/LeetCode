88eaf95ccc9df9f1b25d14aaea84c9f3bf6f23d0
Safwan Mohammed Hanif <109622013+Safwan-Mohammed@users.noreply.github.com>
2026-01-12 17:59:31 +0530

    Add files via upload

---
+++ 119. Pascal's Triangle II/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> prev = new ArrayList<>();
+
+        for(int i = 0; i <= rowIndex; i++){
+            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i) row.add(1);
+                else row.add(prev.get(j - 1) + prev.get(j));
+            }
+            if(i == rowIndex) return row;
+            prev = row;
+        }
+        return prev;
+    }
+}
